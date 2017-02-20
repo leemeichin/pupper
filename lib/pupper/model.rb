ffff716da940d054050952ec1ec56d1821d7833e
Lee Machin <lee@typeform.com>
2017-02-20 18:29:22 +0100

    Get rid of exception handling

--- lib/pupper/model.rb
+++ lib/pupper/model.rb
@@ -66,21 +66,6 @@ module Pupper
       def backend
         return if static?
         @backend ||= "#{model_name.name.pluralize}Client".constantize.new
-      rescue NameError
-        raise NoSuchBackend, <<-ERR
-          Model #{model_name.name} is looking for an API client that doesn't exist!
-
-          Either a) implement the new client:
-
-            # app/api_clients/#{model_name.name.sub('::', '/').downcase.pluralize}_client.rb
-            class #{model_name.name.pluralize}Client < Pupper::Backend
-            end
-
-          Or b) use a different client instead
-
-            self.backend = Other::BackendClient
-
-        ERR
       end
     end
 
