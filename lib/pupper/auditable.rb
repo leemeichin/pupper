393f29780574af049accb1363e2cc2b38bc1beca
Yurii Stakhiv <yurii.stakhiv@typeform.com>
2017-08-23 17:22:15 +0200

    Added failed action audit logs. Added arbitrary audit action logging. Updated specs.

--- lib/pupper/auditable.rb
+++ lib/pupper/auditable.rb
@@ -17,13 +17,33 @@ module Pupper
 
         prepend Module.new { module_eval(underlying_methods, __FILE__, __LINE__) }
       end
+
+      def audit_action(*methods)
+        underlying_methods = ''
+
+        methods.each do |meth|
+          underlying_methods << <<-RB.strip_heredoc
+            def #{meth}
+              begin
+                super
+              rescue Exception => e
+                log_action_failure('#{meth}', e)
+                throw e
+              end
+              log_action '#{meth}'
+            end
+          RB
+        end
+
+        prepend Module.new { module_eval(underlying_methods, __FILE__, __LINE__) }
+      end
     end
 
     included do
       extend ActiveModel::Callbacks
 
-      define_model_callbacks :update, only: :after
-      after_update :create_audit_log
+      define_model_callbacks :update, only: :around
+      around_update :log_update
 
       def audit(&block)
         run_callbacks :update, &block
@@ -34,24 +54,51 @@ module Pupper
         audit_model.where(auditable_type: model_name.name, auditable_id: primary_key)
       end
 
-      def create_audit_log
+      def log_update
+        begin
+          yield
+        rescue Exception => e
+          create_audit_log('update', e)
+          throw e
+        end
+        create_audit_log 'update'
+      end
+
+      def create_audit_log(action, e = nil)
         return unless changed?
 
         audit_model.create(
+          action: action,
           auditable_type: model_name.name,
           auditable_id: primary_key,
           user: Pupper.config.current_user,
-          what_changed: changes
+          what_changed: changes,
+          success: e.nil?,
+          exception: e
+        )
+      end
+
+      def log_action(action, changes = nil, e = nil)
+        audit_model.create(
+          action: action,
+          auditable_type: model_name.name,
+          auditable_id: primary_key,
+          user: Pupper.config.current_user,
+          what_changed: changes,
+          success: e.nil?,
+          exception: e
         )
       end
 
       def update_attributes(attrs)
+        resp = {}
         run_callbacks(:update) do
           assign_attributes(attrs)
-          backend.update
+          resp = backend.update
         end
 
         changes_applied
+        resp
       end
 
       private
