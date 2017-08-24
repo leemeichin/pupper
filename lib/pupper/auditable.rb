8e508b2b7806ad01207bf10d809265edfc82e3a5
Yurii Stakhiv <yurii.stakhiv@typeform.com>
2017-08-24 12:35:18 +0200

    Excluded failed audit log entries from default `audit_logs` results.

--- lib/pupper/auditable.rb
+++ lib/pupper/auditable.rb
@@ -51,7 +51,7 @@ module Pupper
       end
 
       def audit_logs
-        audit_model.where(auditable_type: model_name.name, auditable_id: primary_key).order(created_at: :desc)
+        audit_model.where(auditable_type: model_name.name, auditable_id: primary_key, success: true).order(created_at: :desc)
       end
 
       def log_update
